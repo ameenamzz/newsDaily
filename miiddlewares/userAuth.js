a00adabf80d4f91170a241d4bf7f2a685853bdc3
ameeenamzz <shoukethameen@gmail.com>
2025-08-25 10:36:59 +0530

    created a middleware for userAuth(checking token)

--- miiddlewares/userAuth.js
+++ miiddlewares/userAuth.js
@@ -1,11 +1,22 @@
-const userAuth = (req) => {
-try {
-    
-    
-} catch (error) {
-    
-}
+var jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token is not valid");
+    }
+    const decode = jwt.verify(token, "NEWS@123");
+    const email = decode.email;
+    const user = await User.findOne({
+      email: email,
+    });
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(400).send("ERROR :" + error.message);
+  }
 };
 
 module.exports = { userAuth };
--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,19 +1,11 @@
 const express = require("express");
-const User = require("../models/user");
-const profileRouter = express.Router();
-var jwt = require("jsonwebtoken");
+const { userAuth } = require("../miiddlewares/userAuth");
 
-profileRouter.get("/profile/view", async (req, res) => {
+const profileRouter = express.Router();
+
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
-    const { token } = req.cookies;
-    if (!token) {
-      throw new Error("Token is not valid");
-    }
-    const decode = jwt.verify(token, "NEWS@123");
-    const email = decode.email;
-    const user = await User.findOne({
-      email: email,
-    });
+    const user = req.user;
     res.send(user);
   } catch (error) {
     res.status(400).send("ERROR: " + error.message);
